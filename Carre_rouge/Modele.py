999bd5e81c3d0826098c9a606e3b7c1f87aedaa1
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-02-08 10:46:32 -0500

    canvas

--- Carre_rouge/Modele.py
+++ Carre_rouge/Modele.py
@@ -4,3 +4,5 @@ from Vue import *
 class Modele():
     def __init__(self, controleur):
         self.controleur = controleur
+        self.hauteur = 450
+        self.largeur = 450
--- Carre_rouge/Vue.py
+++ Carre_rouge/Vue.py
@@ -7,3 +7,14 @@ class Vue():
         self.controleur = controleur
         self.modele = modele
         self.root = Tk()
+        self.canvas = Canvas(self.root, height=self.modele.hauteur, width=self.modele.hauteur, borderwidth=50,  bg="black")
+        self.canvas.pack()
+
+        self.canvas2 = Canvas(self.canvas, height=self.modele.hauteur-200, width=self.modele.hauteur-200, borderwidth=50,
+                             bg="red")
+        self.canvas2.pack()
+            # 50 epaisseur
+        # frame = Frame(self.root, height=self.modele.hauteur, width=self.modele.hauteur, bg="seashell2")
+        # frame.pack()
+        # frame1 = Frame(frame, height=200, width=200, bg="red")
+        # frame1.pack()
